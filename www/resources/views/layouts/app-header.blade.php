3fdcf98bffd341d0df6fd59df9dcb5240d70eb36
aziz <ziez0728@gmail.com>
2026-09-08 16:15:42 +0700

    Fix sidebar and header FOUC flash on refresh with x-cloak

--- www/resources/views/layouts/app-header.blade.php
+++ www/resources/views/layouts/app-header.blade.php
@@ -97,7 +97,7 @@
         </div>
 
         <!-- Application Menu (mobile) and Right Side Actions (desktop) -->
-        <div :class="isApplicationMenuOpen ? 'flex' : 'hidden'"
+        <div x-cloak :class="isApplicationMenuOpen ? 'flex' : 'hidden'"
             class="items-center justify-between w-full gap-4 px-5 py-4 xl:flex shadow-theme-md xl:justify-end xl:px-0 xl:shadow-none">
             <div class="flex items-center gap-2 2xsm:gap-3">
                 <!-- Theme Toggle Button -->
--- www/resources/views/layouts/sidebar.blade.php
+++ www/resources/views/layouts/sidebar.blade.php
@@ -7,6 +7,7 @@
 @endphp
 
 <aside id="sidebar"
+    x-cloak
     class="fixed flex flex-col mt-0 top-0 px-5 start-0 bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out z-99999 ltr:border-r rtl:border-l border-gray-200 w-[90px] [.sidebar-expanded_&]:min-w-[290px]"
     x-data="{
         openSubmenus: {},
